852e29bcbc6ac66d4b13f8e7e2308176683f4324
Vivaan Gupta <vgupta18@jh.edu>
2026-04-08 21:45:57 +0000

    export model classes

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,17 +1,19 @@
 """Surgical phase recognition model components."""
 
-from .spatial_encoder import SpatialEncoder, EfficientNetEncoder, ResNetEncoder
+from .spatial_encoder import SpatialEncoder, EfficientNetEncoder, ResNetEncoder, build_encoder
 from .temporal_model import MSTCN, TransSVNet, LSTMTemporalModel
-from .instrument_detector import SurgicalInstrumentDetector
+from .instrument_detector import SurgicalInstrumentDetector, InstrumentPresenceHead
 from .phase_classifier import SurgPhaseClassifier
 
 __all__ = [
     "SpatialEncoder",
     "EfficientNetEncoder",
     "ResNetEncoder",
+    "build_encoder",
     "MSTCN",
     "TransSVNet",
     "LSTMTemporalModel",
     "SurgicalInstrumentDetector",
+    "InstrumentPresenceHead",
     "SurgPhaseClassifier",
 ]
